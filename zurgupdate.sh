5953a8e7f77cf38e8f67b1c08e1a13a6c9f52823
Pukabyte <120460627+Pukabyte@users.noreply.github.com>
2024-01-29 00:58:09 +1300

    Update zurgupdate.sh

--- zurgupdate.sh
+++ zurgupdate.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
-
+echo "Zurg Update Started!"
+echo "Stopping dependant containers..."
 docker stop plex radarr radarr4k radarr4kdv radarranime sonarr sonarr4k sonarr4kdv sonarranime autoscan prowlarr petio petio-mongo rdtclient
 
 cd /opt/zurg-testing
-
+echo "Zurg is coming down..."
 docker compose down
 
-docker image prune -f
+echo "Removing old Zurg images..."
+docker image prune -af
 
-docker volume prune -f
+echo "Removing old Zurg volumes..."
+docker volume prune -af
 
+echo "Composing & updating Zurg..."
 docker compose up -d
 
-echo "Waiting for 60 seconds..."
-sleep 60
+echo "Waiting for Zurg to initialise..."
+sleep 30
 
 docker start plex radarr radarr4k radarr4kdv radarranime sonarr sonarr4k sonarr4kdv sonarranime autoscan prowlarr petio petio-mongo rdtclient
